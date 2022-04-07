e6393d37dd10e9eb580a2b13d089857653266c0c
Isaiah Kang <i.say.yaa@gmail.com>
2022-04-07 17:33:38 -0400

    added some components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Homepage from './components/Homepage.js';
+
+function App() {
+  return (
+    <div>
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import Timeline from "./Timeline"
+import Sidebar from "./Sidebar"
+
+const Homepage = () => {
+    return(
+        <div>
+            <h2>HomePage</h2>
+            <Timeline />
+            <Sidebar/>
+        </div>
+    )
+}
+
+export default Homepage;
